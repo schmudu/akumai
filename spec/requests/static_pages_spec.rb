87f780e928302ca7d3acd0071a7e026fa88cc49d
Patrick Lee <patrick.robin.lee@gmail.com>
2013-10-20 15:35:29 -0700

    Have dynamic title to pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,26 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content("Home")
+    end
+
+    it "should have the title 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("TRIO | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content("Help")
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("TRIO | Help")
     end
   end
 end

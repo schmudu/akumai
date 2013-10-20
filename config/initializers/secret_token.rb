a26eb4aeb538424e2cd03c2b9c3afc036815cca4
Patrick Lee <patrick.robin.lee@gmail.com>
2013-10-20 14:15:11 -0700

    Init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trio::Application.config.secret_key_base = '2e567dcde320b3e8f1109fe52b90304b9d4c11bd332bcef9c42a02cbc04f56b1faa98a617267a2ea04593363bf5e7f8ff9455d0604cce2081a24bf14ff1bda7a'

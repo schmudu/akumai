083c503765309d0d738f5294aeb45b608a72da64
Patrick Lee <patrick.robin.lee@gmail.com>
2013-11-15 18:15:57 -0800

    Added validations for invitation.

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,12 @@
 class Invitation < ActiveRecord::Base
   belongs_to :user
-  has_many :recipients, class_name: "User", foreign_key: "invitation_recipient_id"
+  has_one :recipient, class_name: "User", foreign_key: "invitation_recipient_id"
+
+  validates :user_id, presence: true
+  validate :presence_of_email_or_recipient
+
+  private
+    def presence_of_email_or_recipient
+      errors.add(:email, "email or recipient must be set") if ((recipient.nil?) && (email.blank?))
+    end
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -3,11 +3,45 @@ require_relative '../../app/helpers/constants_helper'
 
 describe Invitation do
   before do
-    @invitation = FactoryGirl.create(:invitation)
+    @user = FactoryGirl.create(:user)
+    @invitation = FactoryGirl.create(:invitation, :user_id => @user.id)
   end
 
   subject { @invitation }
 
   it { should respond_to(:email) }
-  it { should respond_to(:recipients) }
+  it { should respond_to(:recipient) }
+
+  it { should be_valid }
+
+  describe "invalid information" do
+    describe "user id is not set" do
+      before { @invitation.user_id = nil }
+      it { should_not be_valid }
+    end
+
+    describe "email is not set and recipient is not set" do
+      before do 
+        @invitation.email = nil
+      end 
+      it { should_not be_valid }
+    end
+  end
+
+  describe "valid information" do
+    describe "with a recipient" do
+      before do
+        @recipient = FactoryGirl.create(:user)
+        @recipient.invitation_recipient_id = @invitation.id
+      end 
+      
+      it { should be_valid }
+    end
+
+    describe "with an email" do
+      before { @invitation.email = "abc@abc.com" }
+      
+      it { should be_valid }
+    end
+  end
 end

9b488af724782314244dc5a3c66b2b9d139169e8
Patrick Lee <patrick.robin.lee@gmail.com>
2013-11-16 14:39:48 -0800

    Added validations for invitation.

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -5,25 +5,12 @@ class Invitation < ActiveRecord::Base
   belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
 
   validates :sender_id, presence: true
-  validates :recipient_id, presence: true
-  #validate :presence_of_email_or_recipient
-
-  #after_save :save_recipient_reference
+  #validates :recipient_id, presence: true
+  validate :presence_of_email_or_recipient
 
   private
-=begin
     def presence_of_email_or_recipient
-      user_recipient = User.find_by_email(recipient_email)
-      errors.add(:email, "email or recipient must be set") if ((user_recipient.nil?) && (email.blank?))
+      errors.add(:email, "email and recipient cannot both be set") if ((!recipient.nil?) && (!email.blank?))
+      errors.add(:email, "email or recipient must be set") if ((recipient.nil?) && (email.blank?))
     end
-
-    def save_recipient_reference
-      return if recipient_email.nil?
-
-      # save recipient however need to save multiple instances, create a table that relates invitations to user.
-      user_recipient = User.find_by_email(recipient_email)
-      user_recipient.invitation_recipient_id = self.id
-      user_recipient.save
-    end
-=end
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -5,7 +5,7 @@ describe Invitation do
   before do
     @user = FactoryGirl.create(:user)
     @another_user = FactoryGirl.create(:user)
-    @invitation = FactoryGirl.create(:invitation, :sender_id => @user.id, :recipient_id => @another_user.id)
+    @invitation = FactoryGirl.create(:invitation, :sender_id => @user.id, :recipient_id => @another_user.id, :email => nil)
   end
 
   subject { @invitation }
@@ -20,9 +20,46 @@ describe Invitation do
       it { should_not be_valid }
     end
 
-    describe "user id is not set" do
-      before { @invitation.recipient_id = nil }
+    describe "recipient id and email is not set" do
+      before do 
+        @invitation.recipient_id = nil
+        @invitation.email = nil
+      end 
       it { should_not be_valid }
+    end
+
+    describe "recipient id and email is blank" do
+      before do 
+        @invitation.recipient_id = ""
+        @invitation.email = nil
+      end 
+      it { should_not be_valid }
+    end
+
+    describe "recipient id and email are both set" do
+      before do 
+        @invitation.recipient_id = @another_user.id 
+        @invitation.email = "abc@abc.com"
+      end 
+      it { should_not be_valid }
+    end
+  end
+
+  describe "valid information" do
+    describe "should be valid with only email" do
+      before do 
+        @invitation.recipient_id = nil
+        @invitation.email = "abc@abc.com" 
+      end
+      it { should be_valid }
+    end
+
+    describe "should be valid with only recipient id" do
+      before do 
+        @invitation.recipient_id = @another_user.id
+        @invitation.email = nil
+      end
+      it { should be_valid }
     end
   end
 end

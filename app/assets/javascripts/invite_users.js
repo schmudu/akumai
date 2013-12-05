7950ab1896f73afb82cd3b5e9eb395e8d1e292ff
Patrick Lee <patrick.robin.lee@gmail.com>
2013-12-04 20:17:28 -0800

    Added js methods for invitation to users.

--- app/assets/javascripts/invite_users.js
+++ app/assets/javascripts/invite_users.js
@@ -1,33 +1,36 @@
 $(document).ready(function(){
   function enableAllUserLevels(){
-    $("#level_admin").removeClass("disabled");
-    $("#level_staff").removeClass("disabled");
-    $("#level_student").removeClass("disabled");
+    $(".admin").removeClass("disabled");
+    $(".staff").removeClass("disabled");
+    $(".student").removeClass("disabled");
   }
 
   function enableOnlyStudent(){
-    //admin_check = $("#level_admin").is(':checked');
-    if($("#level_admin").is(':checked') == true)
-      $("#level_admin").prop('checked', false);
+    uncheckRadioButton("input.admin");
+    uncheckRadioButton("input.staff");
 
-    if($("#level_staff").is(':checked') == true)
-      $("#level_staff").prop('checked', false);
-
-    //alert("only student");
-    $("#level_admin").addClass("disabled");
-    $("#level_staff").addClass("disabled");
-    $("#level_student").removeClass("disabled");
+    $(".admin").addClass("disabled");
+    $(".staff").addClass("disabled");
+    $(".student").removeClass("disabled");
   }
 
   function disableAllUserLevels(){
+    uncheckRadioButton("input.admin");
+    uncheckRadioButton("input.staff");
+    uncheckRadioButton("input.student");
+
     //alert("disable all users");
-    $("#level_admin").addClass("disabled");
-    $("#level_staff").addClass("disabled");
-    $("#level_student").addClass("disabled");
+    $(".admin").addClass("disabled");
+    $(".staff").addClass("disabled");
+    $(".student").addClass("disabled");
+  }
+
+  function uncheckRadioButton(input_id){
+    if($(input_id).is(':checked') == true)
+      $(input_id).prop('checked', false);
   }
 
   $("#program_id").change(function(){
-    //alert("program selected: " + $(this).val());
     $.get("/program_user_level.json", { program:$(this).val() })
       .done( function( data ){
         var user_level = data.level;
@@ -45,4 +48,43 @@ $(document).ready(function(){
         }
       });
   });
+
+  function setFormTextFocus(input_id, default_entry){
+    var current_entry = $(input_id).val();
+
+    if(current_entry == default_entry){
+      // default entry, set class
+      $(input_id).val('');
+      $(input_id).removeClass("form_default_entry");
+      alert("setting to blank.");
+    }
+  }
+
+  function setFormTextBlur(input_id, default_entry){
+    var current_entry = $(input_id).val();
+
+    if((current_entry == default_entry) || (current_entry == '')){
+      // default entry, set class
+      $(input_id).val(default_entry);
+      $(input_id).addClass("form_default_entry");
+    }
+    else{
+      $(input_id).removeClass("form_default_entry");
+    }
+  }
+
+  $("#email_addresses").focus(function(){
+    setFormTextFocus("#email_addresses", EMAIL_ADDRESSES_DEFAULT);
+  });
+
+  $("#email_addresses").click(function(){
+    setFormTextFocus("#email_addresses", EMAIL_ADDRESSES_DEFAULT);
+  });
+
+  $("#email_addresses").blur(function(){
+    setFormTextBlur("#email_addresses", EMAIL_ADDRESSES_DEFAULT);
+  });
+
+  // init methods
+  setFormTextBlur("#email_addresses", EMAIL_ADDRESSES_DEFAULT);
 });

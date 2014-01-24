6ff7618acfcd23f648e0bb1f6f187676e9d22a76
Patrick Lee <patrick.robin.lee@gmail.com>
2014-01-24 07:35:10 -0800

    Refactored invitations controller tests.

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -6,7 +6,6 @@ describe InvitationsController do
     @another_user=FactoryGirl.create(:user, :email => "another_user@def.com")
     @user_email_not_in_program = "outside_user@gef.com"
     @program=FactoryGirl.create(:program)
-    #@role=FactoryGirl.create(:role, user_id:@user.id, program_id:@program.id, level:ConstantsHelper::ROLE_LEVEL_STAFF, student_id: nil)
     @role=FactoryGirl.create(:role, :user_id => @user.id, :program_id => @program.id, :level => ConstantsHelper::ROLE_LEVEL_STAFF, :student_id => nil)
     sign_in @user
   end
@@ -49,43 +48,6 @@ describe InvitationsController do
         session[:invite_users_invitation_type].should be_nil
         session[:invite_users_program].should be_nil
       end
-
-=begin
-      it "with only email_address" do
-        @params[:email_addresses]="abc@abc.com"
-        post :invite_users_address, @params 
-        assigns[:programs].should_not be_nil
-      end
-
-      it "with a user that already has a role" do
-        FactoryGirl.create(:role, :user_id => @another_user.id, :program_id => @program.id, :level => ConstantsHelper::ROLE_LEVEL_STUDENT)
-        @params[:program_id] = @program.slug
-        @params[:invitation_type]="0"
-        @params[:email_addresses]=@another_user.email
-        post :invite_users_address, @params 
-        assigns[:errors].should_not be_empty
-      end
-
-      it "with a user id that already has an invitation with a sent status" do
-        FactoryGirl.create(:invitation, :sender_id => @user.id, :recipient_id => @another_user.id, :recipient_email => nil, :program_id => @program.id, :user_level => ConstantsHelper::ROLE_LEVEL_STUDENT, :status => ConstantsHelper::INVITATION_STATUS_SENT)
-        @params[:program_id] = @program.slug
-        @params[:invitation_type]="0"
-        @params[:email_addresses]=@another_user.email
-        post :invite_users_address, @params 
-        assigns[:errors].should_not be_empty
-      end
-=end
-
-=begin
-      it "with a user email that already has an invitation with a sent status" do
-        FactoryGirl.create(:invitation, :sender_id => @user.id, :recipient_id => nil, :recipient_email => @user_email_not_in_program, :program_id => @program.id, :user_level => ConstantsHelper::ROLE_LEVEL_STUDENT, :status => ConstantsHelper::INVITATION_STATUS_SENT)
-        @params[:program_id] = @program.slug
-        @params[:invitation_type]="0"
-        @params[:email_addresses]=@user_email_not_in_program
-        post :invite_users_address, @params 
-        assigns[:errors].should_not be_empty
-      end
-=end
     end
 
     describe "with valid params" do
@@ -139,6 +101,29 @@ describe InvitationsController do
         session[:invite_users_email_addresses].should be_nil
       end
 
+      it "with a user that already has a role" do
+        FactoryGirl.create(:role, :user_id => @another_user.id, :program_id => @program.id, :level => ConstantsHelper::ROLE_LEVEL_STUDENT)
+        @params[:email_addresses]=@another_user.email
+        post :review_invitations, @params
+        flash[:invite_users_email_addresses].should == @another_user.email
+        session[:invite_users_email_addresses].should be_nil
+      end
+
+      it "with a user id that already has an invitation with a sent status" do
+        FactoryGirl.create(:invitation, :sender_id => @user.id, :recipient_id => @another_user.id, :recipient_email => nil, :program_id => @program.id, :user_level => ConstantsHelper::ROLE_LEVEL_STUDENT, :status => ConstantsHelper::INVITATION_STATUS_SENT)
+        @params[:email_addresses]=@another_user.email
+        post :review_invitations, @params
+        flash[:invite_users_email_addresses].should == @another_user.email
+        session[:invite_users_email_addresses].should be_nil
+      end
+
+      it "with a user email that already has an invitation with a sent status" do
+        FactoryGirl.create(:invitation, :sender_id => @user.id, :recipient_id => nil, :recipient_email => @user_email_not_in_program, :program_id => @program.id, :user_level => ConstantsHelper::ROLE_LEVEL_STUDENT, :status => ConstantsHelper::INVITATION_STATUS_SENT)
+        @params[:email_addresses]=@user_email_not_in_program
+        post :review_invitations, @params
+        flash[:invite_users_email_addresses].should == @user_email_not_in_program
+        session[:invite_users_email_addresses].should be_nil
+      end
     end
 
     describe "student invitation path" do
@@ -155,39 +140,25 @@ describe InvitationsController do
   end
 
 =begin
+  describe "POST review_invitations" do
+    describe "non-student invitation path" do
+      before do
+        @params = {}
+        @emails = "abc@abc.com, def@def.com"
+        session[:invite_users_program] = @program.slug
+        session[:invite_users_invitation_type]=ConstantsHelper::ROLE_LEVEL_STAFF
+      end
+
+      describe "click submit" do
+      end
+    end
+  end
   describe "POST 'send_invitations'" do
     before(:each) do
       @params = {}
     end
 
     describe "with invalid params" do
-      it "without any parameters" do
-        post :send_invitations, @params
-        assigns[:programs].should_not be_nil
-        assigns[:errors].should_not be_nil
-      end
-
-      it "with only program_id" do
-        @params[:program_id] = @program.slug
-        post :send_invitations, @params 
-        assigns[:programs].should_not be_nil
-        assigns[:errors].should_not be_nil
-      end
-
-      it "with only invitation_type" do
-        @params[:invitation_type]="2"
-        post :send_invitations, @params 
-        assigns[:programs].should_not be_nil
-        assigns[:errors].should_not be_nil
-      end
-
-      it "with only email_address" do
-        @params[:email_addresses]="abc@abc.com"
-        post :send_invitations, @params 
-        assigns[:programs].should_not be_nil
-        assigns[:errors].should_not be_nil
-      end
-
       it "with a user that already has a role" do
         FactoryGirl.create(:role, :user_id => @another_user.id, :program_id => @program.id, :level => ConstantsHelper::ROLE_LEVEL_STUDENT)
         @params[:program_id] = @program.slug

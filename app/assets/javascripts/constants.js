f4928f587b1830f73f693709be581bbc16ddad55
Patrick Lee <patrick.robin.lee@gmail.com>
2014-02-28 22:31:46 -0800

    JS for student entries page now adds invitation id to additional rows.

--- app/assets/javascripts/constants.js
+++ app/assets/javascripts/constants.js
@@ -5,6 +5,7 @@ var LEVEL_ROLE_STAFF=1;
 var LEVEL_ROLE_STUDENT=0;
 
 // INVITATIONS
+var INVITATION_NAME_DEFAULT = "Enter invitation name";
 var INVITATION_DEFAULT_NON_STUDENT_EMAIL = "jake@sample.edu, samantha@something.org";
 var STUDENT_INVITATION_DEFAULT_EMAIL = "Student Email";
 var STUDENT_INVITATION_DEFAULT_ID = "Student ID";
--- app/assets/javascripts/invite_users.js
+++ app/assets/javascripts/invite_users.js
@@ -51,7 +51,7 @@ function setInvitationLevel(program_value){
     });
   }
 
-function insertStudentEntryRow(row_number){
+function insertStudentEntryRow(row_number, invitation_id){
   var element_insert = $("<tr>\
     <td>\
       <input class='form_default_entry email_entry' id='invitation_student_entries_attributes_" + row_number +  "_student_email' \
@@ -62,6 +62,9 @@ function insertStudentEntryRow(row_number){
       <input class='form_default_entry id_entry' id='invitation_student_entries_attributes_" + row_number + "_student_id'\
         name='invitation[student_entries_attributes][" + row_number + "][student_id]' \
         type='text' value='Student ID'> \
+      <input id='invitation_student_entries_attributes_" + row_number + "_invitation_id'\
+        name='invitation[student_entries_attributes][" + row_number + "][invitation_id]' \
+        type='hidden' value='" + invitation_id + "'> \
     </td>");
   $("#student_entries").append(element_insert);
 
@@ -139,12 +142,15 @@ $(document).ready(function(){
     var index_end = last_entry_id.indexOf(string_end);
     var last_row_num = last_entry_id.substr(index_start, index_end-index_start);
 
+    // invitation id
+    var invitation_id = last_entry_row.children().eq(1).children().eq(1).attr("value");
+
     // insert row (5 of them)
-    insertStudentEntryRow(eval(Number(last_row_num)+1));
-    insertStudentEntryRow(eval(Number(last_row_num)+2));
-    insertStudentEntryRow(eval(Number(last_row_num)+3));
-    insertStudentEntryRow(eval(Number(last_row_num)+4));
-    insertStudentEntryRow(eval(Number(last_row_num)+5));
+    insertStudentEntryRow(eval(Number(last_row_num)+1), invitation_id);
+    insertStudentEntryRow(eval(Number(last_row_num)+2), invitation_id);
+    insertStudentEntryRow(eval(Number(last_row_num)+3), invitation_id);
+    insertStudentEntryRow(eval(Number(last_row_num)+4), invitation_id);
+    insertStudentEntryRow(eval(Number(last_row_num)+5), invitation_id);
   });
 
   $(".btn-cancel").attr('href', '#');
@@ -193,6 +199,18 @@ $(document).ready(function(){
     else{
       $(this).removeClass("form_default_entry");
     }
+  });
+
+  $("#invitation_name").focus(function(){
+    setFormTextFocus("#invitation_name", INVITATION_NAME_DEFAULT);
+  });
+
+  $("#invitation_name").click(function(){
+    setFormTextFocus("#invitation_name", INVITATION_NAME_DEFAULT);
+  });
+
+  $("#invitation_name").blur(function(){
+    setFormTextBlur("#invitation_name", INVITATION_NAME_DEFAULT);
   });
 
   $("#email_addresses").focus(function(){

7902e5a7e1136c9da8ca42b88b25f0ba54a8db2c
Patrick Lee <patrick.robin.lee@gmail.com>
2014-03-19 23:53:56 -0700

    Small changes to environment file.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -33,6 +33,7 @@ Trio::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.compress = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true

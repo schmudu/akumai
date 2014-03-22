e169732f897bb155b4fbc05d616e930c03116213
Patrick Lee <patrick.robin.lee@gmail.com>
2014-03-22 12:03:23 -0700

    Using environment variables to store email settings.

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -4,13 +4,13 @@ class InviteMailer < ActionMailer::Base
 
   def send_user_registered invite
     @invite = invite
-    mail(to: invite.email, subject: "Does this work?")
+    mail(to: invite.email, subject: "TRIO - Invitation")
   end
 
   def send_user_unregistered invite
     query_params = {id: invite.slug, code: invite.code, email: invite.email}.to_query
     @link = "#{invites_signup_url}?#{query_params}"
     @invite = invite
-    mail(to: invite.email, subject: "Does this unregistered work?")
+    mail(to: invite.email, subject: "TRIO - Invitation")
   end
 end

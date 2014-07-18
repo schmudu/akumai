7f6b4262f23884f927c7a38e8b65e8748800d787
Patrick Lee <patrick.robin.lee@gmail.com>
2014-07-17 22:31:06 -0700

    Got close to a working version of index helper for analytics.

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -9,7 +9,23 @@ class AnalyticsController < ApplicationController
     programs = current_user.programs
     dataset_entries = []
     programs.each do |program|
-      dataset_entries.concat(program.dataset_entries.select("data, dataset_id, date, role_id").to_a)
+      results = MappedCourse.joins(:dataset_entries)
+          .where("mapped_courses.program_id=?", program.id)
+          .select("dataset_entries.data as data, 
+                    dataset_entries.created_at as date, 
+                    dataset_entries.id as id, 
+                    mapped_courses.name as course_name")  
+      #dataset_entries.concat(program.dataset_entries.select("data, dataset_id, date, role_id").to_a)
+      dataset_entries.concat(results.to_a)
+
+=begin
+results = MappedCourse.joins(:dataset_entries)
+    .where("mapped_courses.program_id=?", 2)
+    .select("dataset_entries.data as data, 
+              dataset_entries.created_at as date, 
+              dataset_entries.id as id, 
+              mapped_courses.name as course_name")  
+=end
     end
     render json: dataset_entries
   end

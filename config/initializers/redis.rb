865a76e328ad9a431946de49ee25e1470b61e02b
Patrick Lee <patrick.robin.lee@gmail.com>
2014-09-09 22:19:03 -0700

    Corrected redis cloud config file.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 if ENV["REDISCLOUD_URL"]
-    uri = URI.parse(ENV["REDISCLOUD_URL"])
-    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    Resque.redis = REDIS
+    #uri = URI.parse(ENV["REDISCLOUD_URL"])
+    #REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    #Resque.redis = REDIS
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+    Resque.redis = $redis
 end

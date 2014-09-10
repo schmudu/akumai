4d1d0108ddeec1c5cd7c188500cd7a86824288e7
Patrick Lee <patrick.robin.lee@gmail.com>
2014-09-09 22:10:44 -0700

    Reverted redis config file.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
 if ENV["REDISCLOUD_URL"]
-    #uri = URI.parse(ENV["REDISCLOUD_URL"])
-    #REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     Resque.redis = REDIS
 end

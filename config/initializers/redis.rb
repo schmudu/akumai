9fd5460c70c79c73a19b364eed7d61143f795b6b
Patrick Lee <patrick.robin.lee@gmail.com>
2014-09-10 21:51:13 -0700

    Got analytics to work and redis as well.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,4 @@
 if ENV["REDISCLOUD_URL"]
-    #uri = URI.parse(ENV["REDISCLOUD_URL"])
-    #REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    #Resque.redis = REDIS
     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
     Resque.redis = $redis
 end

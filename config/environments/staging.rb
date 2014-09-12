cb96054d204ef065040133feb342fb1fd63e3f1f
Patrick Lee <patrick.robin.lee@gmail.com>
2014-09-11 22:12:13 -0700

    Change on configuration to precompile html assets.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,8 +64,7 @@ Trio::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-  config.serve_static_assets = true
+  config.assets.precompile += %w(core-course-checkbox.html  student-checkbox.html)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
